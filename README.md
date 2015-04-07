0c9cac5025d3f361d62a0193879b0d77281bcf77
Tongliang Liu <tliu3@illumina.com>
2015-04-07 13:30:06 -0700

    added Java & Tomcat docker build

---
+++ README.md
@@ -0,0 +1,11 @@
+# A collection of Docker builds for common components
+
+## Java
+- Based on `centos:centos7`
+- Using Oracle Java 8 Server JRE
+
+## Tomcat
+- Based on `cooniur/java:latest`, which is the above Java docker
+- Export `logs`, `temp` and `work` as volumes
+- Expose 8080 port
+- Removed management UI to avoid security issue
---
+++ java/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DOCKER_NAME="cooniur/java"
+
+DOCKER_LOCAL_TAG="$DOCKER_NAME:8u40-b26"
+
+DOCKER_REGISTRY="$1"
+DOCKER_REGISTRY_TAG="$DOCKER_REGISTRY$DOCKER_LOCAL_TAG"
+DOCKER_REGISTRY_LATEST_TAG="$DOCKER_REGISTRY$DOCKER_NAME:latest"
+
+echo -e "\x1B[1;32m---- Building docker image \x1B[1;33m[$DOCKER_LOCAL_TAG]\x1B[1;32m...\x1B[0m"
+docker build --force-rm=true --pull=true --tag="$DOCKER_LOCAL_TAG" -f Dockerfile "$(pwd)"
+
+echo -e "\x1B[1;32m---- Build finished.\x1B[0m"
+echo -e "\x1B[1;32m---- Pushing \x1B[1;33m[$DOCKER_REGISTRY_TAG]\x1B[1;32m and \x1B[1;33m[$DOCKER_REGISTRY_LATEST_TAG]\x1B[1;32m to registry...\x1B[0m"
+docker tag -f $DOCKER_LOCAL_TAG $DOCKER_REGISTRY_TAG
+docker tag -f $DOCKER_LOCAL_TAG $DOCKER_REGISTRY_LATEST_TAG
+
+docker push $DOCKER_REGISTRY_TAG
+docker push $DOCKER_REGISTRY_LATEST_TAG
+
+echo -e "\x1B[1;32m---- Push finished.\x1B[0m"
+echo -e "\x1B[1;32mAll finished.\x1B[0m"
